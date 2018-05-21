e609c804b88d0433cbf39788c20314bf3e296027
Jake Robers <jake@rokkincat.com>
2018-05-21 15:52:57 -0500

    remove comment

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const app = express()
 const transport = new timber.transports.HTTPS(process.env.TIMBER_KEY);
 timber.install(transport);
 
-/**
+// This will log to the Timber dashboard
 console.log(`hello there! Listening on ${process.env.PORT}`)
 console.warn("Payment rejected", {
   event: {
     payment_rejected: { customer_id: "abcd1234", amount: 100, reason: "Card expired" }
   }
 });
- **/
+
 app.use(timber.middlewares.express())
 app.listen(process.env.PORT)
+
